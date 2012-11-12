608727680ea80446291404b3b8d393d7d7b85084
Marc Urben <aegnor@mittelerde.ch>
2012-11-13 00:52:28 +0100

    raw config parser and ini file implemented

--- src/node/config.py
+++ src/node/config.py
@@ -1,9 +1,26 @@
+import os
+from ConfigParser import RawConfigParser
 
 class Config (object):
 	def __init__(self, filename = 'config.ini'):
-		self.filename = filename
+		self.filename = '../config/' + filename
+		config = RawConfigParser()
 
-		self.values = { 'broker' : { 'host' : 'localhost', 'port' : '5672', 'user' : 'test', 'password' : 'test', 'vhost' : 'test'  }  }
+		try:
+			if os.path.isfile(self.filename):
+				config.read(self.filename)
+			elif os.path.isfile('../config/config.ini'):
+				config.read('../config/config.ini')
+		except:
+			print("No cofiguration file found")
+			quit()
+			
+		
+		self.values = { 'broker' : { 'host' : config.get('broker','host'),
+									 'port' : config.get('broker','port'),
+									 'user' : config.get('broker','user'),
+									 'password' : config.get('broker','password'),
+									 'vhost' : config.get('broker','vhost') }}
 
 
 	def load (self):

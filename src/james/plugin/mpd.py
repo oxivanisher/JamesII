f41ce7862b0491d1e281d2770da358f264893721
Marc Urben <aegnor@mittelerde.ch>
2012-12-09 04:49:48 +0100

    fancy subcommands and message integration

--- src/james/plugin/mpd.py
+++ src/james/plugin/mpd.py
@@ -11,11 +11,7 @@ class MpdPlugin(Plugin):
     def __init__(self, core):
         super(MpdPlugin, self).__init__(core, MpdPlugin.name)
 
-        self.create_command('mpc', self.cmd_mpc, 'interface to mpc')
-        self.create_command('radio_on', self.cmd_radio_on, 'play radio')
-        self.create_command('radio_off', self.cmd_radio_off, 'stop radio')
-        self.create_command('mpd_sleep', self.cmd_mpd_sleep, 'play sleep radio and fade slowly out')
-        self.create_command('mpd_wakeup', self.cmd_mpd_wakeup, 'play wakeup radio and fade slowly in')
+        self.create_command('mpd', self.cmd_mpd, 'interface to mpd via mpc')
 
         self.mpc_bin = '/usr/bin/mpc'
 
@@ -27,23 +23,52 @@ class MpdPlugin(Plugin):
         if self.core.config['mpd']['password']:
             self.connection_string += " --password=" + self.core.config['mpd']['password']
 
-    def terminate(self):
-        pass
+    def cmd_mpd(self, args):
+        sub_commands = {'mpc' : self.mpc,
+                        'radio_on' : self.radio_on,
+                        'radio_off': self.radio_off,
+                        'mpd_sleep': self.mpd_sleep,
+                        'mpd_wakeup': self.mpd_wakeup}
+
+        output = ("subcommands are: %s" % (', '.join(sub_commands.keys())))
+        try:
+            user_command = args[0]
+        except Exception as e:
+            return (output)
+        for command in sub_commands.keys():
+            if command == user_command:
+                return sub_commands[command](args[1:])
+        return (output)        
 
 #if os.path.isfile(self.mpc_bin): FIXME
-    def cmd_mpc(self, args):
+    def mpc(self, args):
         self.exec_mpc(args)
 
-    def cmd_radio_off(self, args):
+    def radio_off(self, args):
+        message = self.core.new_message(self.name)
+        message.header = "MPC: Radio off"
+        message.level = 1
+        message.send()
+
         self.exec_mpc(['clear'])
 
     # radio (online url) methods
 #if self.core.config['mpd']['url'] != "":
-    def cmd_radio_on(self, args):
+    def radio_on(self, args):
+        message = self.core.new_message(self.name)
+        message.header = "MPC: Radio on"
+        message.level = 1
+        message.send()
+
         self.load_online_playlist(self.core.config['mpd']['radio_url'])
         self.exec_mpc(['play'])     
 
-    def cmd_mpd_sleep(self, args):
+    def mpd_sleep(self, args):
+        message = self.core.new_message(self.name)
+        message.header = "MPC: Sleep mode enabled"
+        message.level = 1
+        message.send()
+
         self.load_online_playlist(self.core.config['mpd']['sleep_url'])
         self.exec_mpc(['play'])
         minutes = 1
@@ -53,7 +78,12 @@ class MpdPlugin(Plugin):
             pass
         subprocess.Popen(['/usr/bin/mpfade', str(minutes), "0", self.core.config['mpd']['host']])
 
-    def cmd_mpd_wakeup(self, args):
+    def mpd_wakeup(self, args):
+        message = self.core.new_message(self.name)
+        message.header = "MPC: Wakeup mode enabled"
+        message.level = 1
+        message.send()
+
         self.exec_mpc(['clear'])
         self.load_online_playlist(self.core.config['mpd']['wakeup_url'])
         minutes = 1
@@ -69,14 +99,18 @@ class MpdPlugin(Plugin):
         mpc_pipe = os.popen(self.connection_string + ' ' + ' '.join(args),'r')
         mpc = mpc_pipe.read().strip()
         mpc_pipe.close()
+
+        message = self.core.new_message(self.name)
+        message.header = "MPC: " + ' '.join(args)
+        message.body = mpc
+        message.send()
+
         return mpc
 
     def load_online_playlist(self, url):
         for source in urllib2.urlopen(url):
             if source != "":
                 self.exec_mpc(['add', source])
-
-
 
 descriptor = {
     'name' : 'mpd',

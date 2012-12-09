aaac0149bdaa461418a284c98823691c50ac0329
Marc Urben <aegnor@mittelerde.ch>
2012-12-09 04:54:10 +0100

    fancy subcommands

--- src/james/plugin/raspberry.py
+++ src/james/plugin/raspberry.py
@@ -14,22 +14,36 @@ class RaspberryPlugin(Plugin):
 
         super(RaspberryPlugin, self).__init__(core, RaspberryPlugin.name)
 
-        self.create_command('rasp_test', self.cmd_rasp_test, 'raspberry test')
+        self.create_command('rasp', self.cmd_rasp, 'raspberry api')
+
         self.gpio = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_PINS)
         
         #gpio.pinMode(1,gpio.OUTPUT)
         #gpio.digitalWrite(1,gpio.HIGH)
 
 
-    def terminate(self):
-        pass
+    def cmd_rasp(self, args):
+        sub_commands = {'test' : self.rasp_test}
 
-    def cmd_rasp_test(self, args):
+        output = ("subcommands are: %s" % (', '.join(sub_commands.keys())))
+        try:
+            user_command = args[0]
+        except Exception as e:
+            return (output)
+        for command in sub_commands.keys():
+            if command == user_command:
+                return sub_commands[command](args[1:])
+        return (output)
+
+    def rasp_test(self, args):
         self.gpio.pinMode(1,self.gpio.OUTPUT)
         self.gpio.digitalWrite(1,self.gpio.HIGH)
+
+        message = self.core.new_message(self.name)
+        message.header = "Testing RaspberryPi"
+        message.send()
+        
         pass
-
-
 
 descriptor = {
     'name' : 'raspberry',

ef258563632a0a352c3cdf00363f47f53bc819cd
Marc Urben <aegnor@mittelerde.ch>
2012-12-25 17:21:56 +0100

    i have no command anymore

--- src/james/plugin/dbus-notify.py
+++ src/james/plugin/dbus-notify.py
@@ -14,6 +14,7 @@ class DbusNotifyPlugin(Plugin):
 
         self.bus = dbus.Bus(dbus.Bus.TYPE_SESSION)
         self.notifyid = 0
+        self.commands = []
 
     def process_message(self, message):
         # Connect to notification interface on DBUS.

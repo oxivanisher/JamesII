815a8c1a64a23e4e624ca68fc0f639cb62c12eaa
Marc Urben <aegnor@mittelerde.ch>
2012-12-28 02:55:33 +0100

    proximity now really detects persons :)

--- src/james/plugin/proximity.py
+++ src/james/plugin/proximity.py
@@ -15,7 +15,9 @@ class ProximityPlugin(Plugin):
 
         self.status = False
         self.hosts_online = []
+        self.persons_status = {}
 
+        self.commands.create_subcommand('persons', 'shows the persons currently detected', self.show_persons)
         if os.path.isfile('/usr/bin/hcitool'):
             self.commands.create_subcommand('discover', 'scan for visible bluetooth devices', self.discover)
             self.commands.create_subcommand('test', 'test for local bluetooth devices', self.test)
@@ -23,8 +25,12 @@ class ProximityPlugin(Plugin):
                 self.commands.create_subcommand('proximity', 'run a manual proximity check', self.proximity_check)
                 self.proximity_check_daemon()
 
+        for person in self.core.config['persons'].keys():
+            self.persons_status[person] = False
+
         atexit.register(self.save_state)
         self.state_file = os.path.join(os.path.expanduser("~"), ".james_proximity_state")
+        self.persons_status_file = os.path.join(os.path.expanduser("~"), ".james_persons_status")
         self.proxy_send_lock = False
         self.load_saved_state()
 
@@ -41,10 +47,18 @@ class ProximityPlugin(Plugin):
             else:
                 self.core.proximity_status.status[self.core.location] = True
             self.core.proximity_event(loaded_status, 'btproximity')
-
         except IOError:
             pass
         pass
+
+        try:
+            file = open(self.persons_status_file, 'r')
+            self.persons_status = self.core.utils.convert_from_unicode(json.loads(file.read()))
+            file.close()
+        except IOError:
+            pass
+        pass
+
 
     def save_state(self):
         try:
@@ -54,8 +68,18 @@ class ProximityPlugin(Plugin):
             if self.core.config['core']['debug']:
                 print("Saving proximity status to %s" % (self.state_file))
         except IOError:
-            print("WARNING: Could not safe cached commands to file!")
+            print("WARNING: Could not safe proximity status to file!")
 
+        try:
+            file = open(self.persons_status_file, 'w')
+            file.write(json.dumps(self.persons_status))
+            file.close()
+            if self.core.config['core']['debug']:
+                print("Saving persons status to %s" % (self.persons_status_file))
+        except IOError:
+            print("WARNING: Could not safe persons status to file!")
+
+    # command methods
     def test(self, args):
         devices = {}
         lines = self.core.utils.popenAndWait(['hcitool', 'dev'])
@@ -76,6 +100,17 @@ class ProximityPlugin(Plugin):
                 hosts[values[0]] = values[1]
         return(hosts)
 
+    def show_persons(self, args):
+        ret = []
+        print self.persons_status
+        for person in self.persons_status.keys():
+            if self.persons_status[person]:
+                ret.append("%10s is here" % (person))
+            else:
+                ret.append("%10s is not here" % (person))
+        return ret
+
+    # proximity daemon methods
     def proximity_check_daemon(self):
         self.proximity_check(None)
         sleep = self.core.config['proximity']['sleep_short']
@@ -90,8 +125,8 @@ class ProximityPlugin(Plugin):
     def proximity_check_worker(self):
         hosts = []
         for person in self.core.config['persons'].keys():
-            for name in self.core.config['persons'][person]['devices'].keys():
-                mac = self.core.config['persons'][person]['devices'][name]
+            for name in self.core.config['persons'][person]['bt_devices'].keys():
+                mac = self.core.config['persons'][person]['bt_devices'][name]
                 ret = self.core.utils.popenAndWait(['/usr/bin/hcitool', 'info', mac])
                 clear_list = filter(lambda s: s != '', ret)
                 try:
@@ -108,6 +143,12 @@ class ProximityPlugin(Plugin):
         self.status = False
         old_hosts_online = self.hosts_online
         new_hosts_online = []
+        persons_detected = []
+        new_persons_status = {}
+
+        # resetting persons detected
+        for person in self.core.config['persons'].keys():
+            new_persons_status[person] = False
 
         if len(values) > 0:
             self.status = True
@@ -133,8 +174,36 @@ class ProximityPlugin(Plugin):
             if notfound:
                 self.send_response(self.uuid, 'broadcast', ('Proximity lost %s' % (name)))
 
+        # registering the person for this device as detected
+        for (mac, name) in values:
+            for person in self.core.config['persons'].keys():
+                for device in self.core.config['persons'][person]['bt_devices'].values():
+                    if device == mac:
+                        persons_detected.append(person)
+                        new_persons_status[person] = True
+
+        # checking for newly detected persons
+        for person in new_persons_status.keys():
+            try:
+                self.persons_status[person]
+            except KeyError:
+                # compensating for config changes
+                self.persons_status[person] = False
+
+            if new_persons_status[person] != self.persons_status[person]:
+                message = self.core.new_message(self.name)
+                message.level = 1
+                if new_persons_status[person]:
+                    message.header = ("%s is now here" % person)
+                else:
+                    message.header = ("%s left" % person)
+                message.body = ("Location: %s" % self.core.location)
+                message.send()
+
         # save the actual online hosts to var
         self.hosts_online = new_hosts_online
+        # saving the actual persons detected
+        self.persons_status = new_persons_status
 
         if self.status != self.oldstatus:
             if self.status:

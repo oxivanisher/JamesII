077cc5525558f1a5f994347d6e4b6aaaa28ce385
Marc Urben <aegnor@mittelerde.ch>
2012-12-30 06:01:25 +0100

    better return

--- src/james/plugin/system.py
+++ src/james/plugin/system.py
@@ -24,8 +24,8 @@ class SystemPlugin(Plugin):
             self.commands.create_subcommand('version', 'Shows the git checkout HEAD', self.cmd_version)
 
     def get_ip(self, args):
-        return commands.getoutput("/sbin/ifconfig | grep -i \"inet\" | grep -iv \"inet6\" | " +
-                         "awk {'print $2'} | sed -ne 's/addr\:/ /p' | grep -v '127.0.0.1'")
+        return commands.getoutput(["/sbin/ifconfig | grep -i \"inet\" | grep -iv \"inet6\" | " +
+                         "awk {'print $2'} | sed -ne 's/addr\:/ /p' | grep -v '127.0.0.1'"])
 
     def start(self):
         try:
@@ -47,9 +47,9 @@ class SystemPlugin(Plugin):
         pass
 
     def show_proximity(self, args):
-        return ("%-10s %-10s %s" % (self.core.hostname,
+        return (["%-10s %-10s %s" % (self.core.hostname,
                                   self.core.proximity_status.get_status_here(),
-                                  self.core.location))
+                                  self.core.location)])
 
     def show_plugins(self, args):
         plugin_names = []
@@ -63,7 +63,7 @@ class SystemPlugin(Plugin):
         version_pipe = os.popen('/usr/bin/git log -n 1 --pretty="format:%h %ci"')
         version = version_pipe.read().strip()
         version_pipe.close()
-        return version
+        return [version]
 
     def cmd_message(self, args):
         message_string = ' '.join(args)
@@ -79,9 +79,9 @@ class SystemPlugin(Plugin):
         try:
             message.header = message_list[0].strip()
             message.send()
-            return ("Message header: %s; body: %s" % (message.header, message.body))
+            return (["Message header: %s; body: %s" % (message.header, message.body)])
         except Exception as e:
-            return ("Message could not me sent (%s)" % (e))
+            return (["Message could not me sent (%s)" % (e)])
 
     def cmd_quit(self, args):
         message = self.core.new_message(self.name)

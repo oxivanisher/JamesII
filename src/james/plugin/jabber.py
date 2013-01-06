4c879230b59e179ec666c28e1e0ecd2b161a206f
Marc Urben <aegnor@mittelerde.ch>
2013-01-06 04:07:40 +0100

    basic stuff working except help

--- src/james/plugin/jabber.py
+++ src/james/plugin/jabber.py
@@ -1,36 +1,101 @@
 
 import xmpp
+import sys
 
 from james.plugin import *
 
 # http://xmpppy.sourceforge.net/
+# FIXME add muc support and send broadcast command responses only there
 class JabberThread(PluginThread):
 
-    def __init__(self, plugin, cfg_jid, password):
+    def __init__(self, plugin, users, cfg_jid, password):
         # FIXME i must become a singleton!
         super(JabberThread, self).__init__(plugin)
         self.cfg_jid = cfg_jid
         self.password = password
-        active = True
+        self.active = True
+        self.users = users
 
     def work(self):
-        jid=xmpp.protocol.JID(self.cfg_jid)
-        cl=xmpp.Client(jid.getDomain(),debug=[])
+        # setup connection
+        jid = xmpp.protocol.JID(self.cfg_jid)
+        server = jid.getDomain()
+        conn = xmpp.Client(server,debug=[])
+        conres = conn.connect()
 
-        cl.connect()
-        cl.auth(jid.getNode(),self.password)
+        if not conres:
+            print "Unable to connect to server %s!"%server
+            self.active = False
+        if conres != 'tls':
+            print "Warning: unable to estabilish secure connection - TLS failed!"
 
-        cl.sendInitialPresence()
-        cl.send(xmpp.protocol.Message("oxi@oxi.ch","my test message"))
+        authres = conn.auth(jid.getNode(), self.password)
+        if not authres:
+            print "Unable to authorize on %s - check login/password."%server
+            self.active = False
+        if authres != 'sasl':
+            print "Warning: unable to perform SASL auth on %s. Old authentication method used!"%server
+
+        # registering handlers
+        conn.RegisterHandler('message',self.message_callback)
+
+        # lets go online
+        conn.sendInitPresence()
+
+        # dive into the endless loops
+        self.GoOn(conn)
+
+    def StepOn(self, conn):
+        conn.Process(1)
 
         self.plugin.worker_lock.acquire()
+
         # see if i must shut myself down
         if self.plugin.worker_exit:
-            active = False
+            self.active = False
+        # see if we must send messages
+        for (header, body, to_jid) in self.plugin.waiting_messages:
+            try:
+                message = False
+                if to_jid:
+                    # message to one user
+                    message = self.create_message(to_jid, header, body)
+                    conn.send(message)
+                else:
+                    # broadcast message to every user
+                    for (jid, name) in self.users:
+                        message = self.create_message(jid, header, body)
+                        conn.send(message)
+            except Exception as e:
+                print("Jabber worker ERROR: %s" % e)
+
+        self.plugin.waiting_messages = []
         self.plugin.worker_lock.release()
 
-    def on_exit(self, result):
-        pass
+        if not self.active:
+            return 0
+        else:
+            return 1
+
+    def create_message(self, to, header = [], body = []):
+        if len(header) == 0:
+            return False
+        message_list = header
+        if len(body) > 0:
+            message_list.append('\n')
+            message_list = message_list + body
+        message_text = '\n'.join(message_list)
+        message = xmpp.protocol.Message(to, message_text)
+        message.setAttr('type', 'chat')
+        return message
+
+    def GoOn(self, conn):
+        while self.StepOn(conn): pass
+
+    # callback handlers
+    def message_callback(self, conn, message):
+        print("message callback")
+        self.plugin.core.add_timeout(0, self.plugin.process_jabber_message, message)
 
     # called when the worker ends
     def on_exit(self, result):
@@ -45,11 +110,21 @@ class JabberPlugin(Plugin):
         self.rasp_thread = False
         self.worker_exit = False
         self.worker_lock = threading.Lock()
+        self.waiting_messages = []
+        self.users = []
+        # FIXME please implement me as status of jabber james
+        self.nodes_online_string = ''
 
-        self.commands.create_subcommand('test', 'Do some test', self.cmd_xmpp_test)
+        self.commands.create_subcommand('test', 'Sends a test message over jabber', self.cmd_xmpp_test)
+        self.commands.create_subcommand('list', 'Lists all allowed Jabber users', self.cmd_list_users)
 
     # plugin methods
     def start(self):
+        for person in self.core.config['persons'].keys():
+            try:
+                self.users.append((self.core.config['persons'][person]['jid'], person))
+            except Exception:
+                pass
         self.start_worker()
     
     def terminate(self):
@@ -57,9 +132,70 @@ class JabberPlugin(Plugin):
 
     # james command methods
     def cmd_xmpp_test(self, args):
-        pass
+        self.send_message(["test head"], ["test body line 1", "test body line"])
+        return "Sending test message"
+
+    def cmd_list_users(self, args):
+        ret = []
+        for (jid, name) in self.users:
+            ret.append("%-15s %s" % (jid, name))
+        return ret
 
     # methods for worker process
+    def send_message(self, message_head = [], message_body = [], to = None):
+        print ("s head: %s" % message_head)
+        print ("s body: %s" % message_body)
+        self.worker_lock.acquire()
+        self.waiting_messages.append((message_head, message_body, to))
+        self.worker_lock.release()
+
+    def process_jabber_message(self, message):
+        jid_data = str(message.getFrom()).split('/')
+        jid_from = jid_data[0]
+        jid_ress = jid_data[1]
+
+        command = self.core.utils.convert_from_unicode(message.getBody().split())
+        print("command: %s" % command)
+
+        print("jid_from: %s" % jid_from)
+        print ("process in main thread")
+        if 'help' in message.getBody():
+            search_for = self.core.utils.convert_from_unicode(message.getBody().split())
+            print("search for: %s" % search_for)
+            help_text = self.jabber_cmd_help(command)
+            print("help text: %s" % help_text)
+            self.send_message(['Commands are:'], help_text, jid_from)
+        else:
+            self.send_command(command)
+        pass
+
+    # worker process help functions
+    def jabber_cmd_help(self, args):
+        ret = []
+        if len(args) > 0:    
+            command = self.core.ghost_commands.get_best_match(args)
+            print command
+            if command:
+                ret.append("%s:" % (command.help))
+                self.print_command_help_lines(command)
+            else:
+                ret.append("Command not found")
+        else:
+            ret.append("%-20s %s" % ('Command:', 'Description:'))
+            ret = ret + self.print_command_help_lines(self.core.ghost_commands, 1)
+        return ret
+
+    def print_command_help_lines(self, command_obj, depth = 0):
+        ret = []
+        for command in sorted(command_obj.subcommands.keys()):
+            c = command_obj.subcommands[command]
+            if not c.hide:
+                ret.append("|%-19s %s" % (depth * "-" + " " + c.name, c.help))
+                print("|%-19s %s" % (depth * "-" + " " + c.name, c.help))
+            if len(c.subcommands.keys()) > 0:
+                ret = ret + self.print_command_help_lines(c, depth + 1)
+        return ret
+
     def on_worker_exit(self):
         self.send_broadcast(['XMPP worker exited'])
 
@@ -70,8 +206,9 @@ class JabberPlugin(Plugin):
         self.worker_exit = False
         self.worker_lock.release()
         self.rasp_thread = JabberThread(self,
-                                      self.core.config['xmpp']['jid'],
-                                      self.core.config['xmpp']['password'])
+                                        self.users,
+                                        self.core.config['jabber']['jid'],
+                                        self.core.config['jabber']['password'])
         self.rasp_thread.start()
         return self.send_broadcast(['XMPP worker starting'])
 
@@ -80,6 +217,39 @@ class JabberPlugin(Plugin):
         self.worker_exit = True
         self.worker_lock.release()
         return self.send_broadcast(['XMPP worker exiting'])
+
+    # plugin event methods
+    def process_command_response(self, args, host, plugin):
+        message = ['Direct:']
+        for line in args:
+            message.append("%10s@%-10s: %s" % (plugin, host, line))
+        self.send_message(message)
+
+    def process_broadcast_command_response(self, args, host, plugin):
+        message = ['Broadcast:']
+        for line in args:
+            message.append("%10s@%-10s: %s" % (plugin, host, line))
+        self.send_message(message)
+
+    def process_message(self, message):
+        if message.level > 1:
+            message_text = ['Message with level %s from %s@%s:' % (message.level,
+                                                                   message.sender_name,
+                                                                   message.sender_host)]
+            for line in self.core.utils.list_unicode_cleanup([message.header]):
+                message_text.append(line)
+            try:
+                for line in self.core.utils.list_unicode_cleanup([message.body]):
+                    message_text.append(line)
+            except Exception:
+                pass
+            self.send_message(message_text)
+
+    def process_proximity_event(self, newstatus):
+        if self.core.config['core']['debug']:
+            print("Jabber Processing proximity event")
+        if not newstatus['status'][self.core.location]:
+            self.send_message(['Nobody at home. Security measures activated.'])
 
 descriptor = {
     'name' : 'jabber',

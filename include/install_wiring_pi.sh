4e8591386fc1c96e1cda1a6493c72bb574b6c050
Marc Urben <aegnor@mittelerde.ch>
2013-01-15 03:12:29 +0100

    debug

---
+++ include/install_wiring_pi.sh
@@ -0,0 +1,7 @@
+debian:
+sudo apt-get install python-dev build-essential python-setuptools
+git clone git@github.com:WiringPi/WiringPi-Python.git
+cd WiringPi-Python
+git clone git@github.com:WiringPi/WiringPi.git
+git submodule update --init
+sudo python setup.py install

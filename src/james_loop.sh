ed0087ed6a3619e23b2bfe852ad56668342f0124
Marc Urben <aegnor@mittelerde.ch>
2013-03-16 21:47:04 +0100

    dont git pull on connection problems

--- src/james_loop.sh
+++ src/james_loop.sh
@@ -2,28 +2,35 @@
 INPWD=$(pwd)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
+GITPULL=true
 while true;
 do
 	CRASH=false
 
-	clear
-	echo -e "..:: Doing git pull ::..\n"
-	git pull
+	if [[ $GITPULL ]];
+	then
+		clear
+		echo -e "..:: Doing git pull ::..\n"
+		git pull
+	fi
 
 	clear
 	echo -e "..:: Starting james.py ($(date)) ::..\n"
 	sudo ./james.py
 	if [[ $? -eq 2 ]];
 	then
+		GITPULL=false
 		echo -e "\nJamesII connection error detected. Sleeping for 20 seconds\n"
 		sleep 20
 	elif [[ $? -gt 0 ]];
 	then
+		GITPULL=true
 		echo -e "\nJamesII crash detected. Sleeping for 20 seconds\n"
 		echo $(date +%s) > ./.james_crashed
 		chmod 666 ./.james_crashed
 		sleep 20
 	else
+		GITPULL=true
 		echo -e "\nJamesII graceful shutdown detected\n"
 		sleep 1
 	fi

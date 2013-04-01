ef8722f3965a7bedc4f133f617bf1c9d39f24801
Marc Urben <aegnor@mittelerde.ch>
2013-04-02 00:01:29 +0200

    netlog bugfixes

--- src/james/__init__.py
+++ src/james/__init__.py
@@ -136,8 +136,8 @@ class Core(object):
                 pass
 
         # registring network logger handlers
-        if self.config['monitor']['nodes']:
-            for target_host in self.config['netlog$']['nodes']:
+        if self.config['netlog']['nodes']:
+            for target_host in self.config['netlog']['nodes']:
                 socketHandler = logging.handlers.SocketHandler(target_host, logging.handlers.DEFAULT_TCP_LOGGING_PORT)
                 socketHandler.setLevel(logging.DEBUG)
                 self.logger.addHandler(socketHandler)

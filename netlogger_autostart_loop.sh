4cb02773d0c5e0263910c84dae9fcaae7cec60d5
Marc Urben <aegnor@mittelerde.ch>
2013-04-07 20:26:42 +0200

    changed screen name

--- netlogger_autostart_loop.sh
+++ netlogger_autostart_loop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$(which screen) -dmS JamesII $DIR/src/netlogger_loop.sh
+$(which screen) -dmS NetLogger_JamesII $DIR/src/netlogger_loop.sh

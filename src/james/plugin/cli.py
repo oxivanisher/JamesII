3c9059eb36027c934974a8788dd11905b7f3e791
Marc Urben <aegnor@mittelerde.ch>
2013-04-09 18:49:58 +0200

    eye candy

--- src/james/plugin/cli.py
+++ src/james/plugin/cli.py
@@ -179,7 +179,7 @@ class CliPlugin(Plugin):
             display_data.append(('UUID', args['uuid']))
             display_data.append(('IP', args['ip'][0]))
             display_data.append(('James Startup', startup))
-            display_data.append(('Delay', '%s  seconds' % timedelay))
+            display_data.append(('Delay', '%s seconds' % timedelay))
             display_data.append(('Location', args['location']))
             display_data.append(('Platform', args['platform']))
             display_data.append(('OS Username', args['os_username']))

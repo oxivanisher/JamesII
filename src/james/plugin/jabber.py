c6c59062cb9e72ea7d06629fa1b0ddf5844fecd9
Marc Urben <aegnor@mittelerde.ch>
2013-11-18 11:17:17 +0100

    startup delay

--- src/james/plugin/jabber.py
+++ src/james/plugin/jabber.py
@@ -197,7 +197,7 @@ class JabberThread(PluginThread):
 
     # callback handlers
     def message_callback(self, conn, message):
-        if (time.time() - self.startupTime) < 2:
+        if (time.time() - self.startupTime) < 10:
             self.logger.debug("Ignoring message from %s due startup delay check" % (message.getFrom()))
         else:
             realjid = self.cfg_jid

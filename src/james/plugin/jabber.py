9742bdfeea898d6cb2f893c9ce58ad084f564c82
Marc Urben <aegnor@mittelerde.ch>
2013-11-19 11:23:49 +0100

    muc online users

--- src/james/plugin/jabber.py
+++ src/james/plugin/jabber.py
@@ -231,7 +231,9 @@ class JabberThread(PluginThread):
                 admin = None
                 # check if the user is a admin
                 for (jid, username) in self.users:
+                    print "(jid, username): %s %s" % (jid, username)
                     src_jid = self.plugin.utils.convert_from_unicode(jid).split('/')
+                    print "src_jid: %s" % src_jid
                     if src_jid[0] == realjid:
                         admin = username
                         print "admin found on %s" % src_jid[0]

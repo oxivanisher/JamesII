294a05e2e76a9ebcd32da272ad02b35f1b1451c1
Marc Urben <aegnor@mittelerde.ch>
2014-03-12 23:49:04 +0100

    bugfix

--- src/james/plugin/jabber.py
+++ src/james/plugin/jabber.py
@@ -647,7 +647,7 @@ class JabberPlugin(Plugin):
 
     def alert(self, args):
         self.logger.debug('Alerting (%s)' % ' '.join(args))
-        self.send_xmpp_message(' '.join(args))
+        self.send_xmpp_muc_message(' '.join(args))
 
     def return_status(self):
         ret = {}

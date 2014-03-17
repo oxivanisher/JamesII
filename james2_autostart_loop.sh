2f4a49f3b34ffc79f22e153eaae7533fcc551429
Marc Urben <aegnor@mittelerde.ch>
2014-03-17 20:58:08 +0100

    screen vs. upstart

--- james2_autostart_loop.sh
+++ james2_autostart_loop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-sudo /etc/init.d/screen-cleanup start > /dev/null 2>&1
+ls /var/run/screen > /dev/null 2>&1 || sudo /etc/init.d/screen-cleanup start > /dev/null 2>&1
 $(which screen) -dmS JamesII $DIR/src/james_loop.sh

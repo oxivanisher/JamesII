993cda8eb25a1624413419fb2d8c1fcba6f32543
Marc Urben <aegnor@mittelerde.ch>
2014-03-28 00:28:20 +0100

    unicode hell

--- src/james/plugin/google-calendar.py
+++ src/james/plugin/google-calendar.py
@@ -103,7 +103,7 @@ class GoogleCalendarPlugin(Plugin):
             if event['status'] == "tentative":
                 retStr += " possibly "
                 # evil is: 
-            retStr += event['summary'].replace(u"\xc2", "")
+            retStr += event['summary'].replace("\xc2", "")
 
             retList.append(retStr)
 

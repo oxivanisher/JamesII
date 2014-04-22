a59bc0e29659a05945a37aa607157407f22c1448
Marc Urben <aegnor@mittelerde.ch>
2014-04-22 19:28:17 +0200

    typo

--- src/james/plugin/wakeonlan.py
+++ src/james/plugin/wakeonlan.py
@@ -15,8 +15,8 @@ class WakeOnLanPlugin(Plugin):
             try:
                 for name in self.core.config['persons'][person]['eth_devices'].keys():
                     self.wol_devices.append((name, self.core.config['persons'][person]['eth_devices'][name]))
-                except KeyError:
-                    pass
+            except KeyError:
+                pass
 
         self.commands.create_subcommand('list', 'Lists available wol target hosts', self.wol_list)
         self.commands.create_subcommand('wake', 'Wakes up a given host (hostname)', self.wol_wake)

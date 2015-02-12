532beb4ec9e51ad33711e504cc716ede4d79d735
Marc Urben <aegnor@mittelerde.ch>
2015-02-12 20:31:47 +0100

    testing

--- include/install_wiringpi.sh
+++ include/install_wiringpi.sh
@@ -2,8 +2,10 @@
 
 #debian:
 sudo apt-get install -y python-dev build-essential python-setuptools
-git clone https://github.com/WiringPi/WiringPi-Python.git
-cd WiringPi-Python
+#git clone https://github.com/WiringPi/WiringPi-Python.git
+git clone https://github.com/WiringPi/WiringPi2-Python.git
+#cd WiringPi-Python
+cd WiringPi2-Python
 git clone git://git.drogon.net/wiringPi
 git submodule update --init
 cd WiringPi

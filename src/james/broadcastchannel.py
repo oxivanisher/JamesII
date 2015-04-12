c9e2597746d904321bfb307f896ad664a38f2195
Marc Urben <aegnor@mittelerde.ch>
2015-04-12 19:14:25 +0200

    message sending debugging?

--- src/james/broadcastchannel.py
+++ src/james/broadcastchannel.py
@@ -1,12 +1,16 @@
 
 # import pickle
 import json
+import pika
+import time
+import jamesutils
 
 class BroadcastChannel(object):
     def __init__(self, core, name):
         self.core = core
         self.name = name
         self.listeners = []
+        self.log = jamesutils.getLogger('broadcastchannel')
 
         self.channel = self.core.connection.channel()
         self.channel.exchange_declare(exchange=self.name, type='fanout')
@@ -17,8 +21,21 @@ class BroadcastChannel(object):
         self.channel.basic_consume(self.recv, queue=self.queue_name, no_ack=True)
 
     def send(self, msg):
+        msgSent = False
+        tryCount = 0
         body = json.dumps(msg)
-        self.channel.basic_publish(exchange=self.name, routing_key='', body=body)
+        while not msgSent:
+            try:
+                self.channel.basic_publish(exchange=self.name, routing_key='', body=body)
+                msgSent = True
+            except pika.exceptions.ConnectionClosed:
+                tryCount += 1
+                if tryCount >= 20:
+                    self.log.warning("Stopping to send message!")
+                    msgSent = True
+                else:
+                    self.log.info("Unable to send message <%s>. Will retry in 3 sec")
+                    time.sleep(3)
 
     def recv(self, channel, method, properties, body):
         msg = json.loads(body)

17f2e2d7426cc3394ff399b0dc3641ac9048ca75
Marc Urben <aegnor@mittelerde.ch>
2015-04-13 13:11:24 +0200

    fixing uptime

--- src/james/plugin/system.py
+++ src/james/plugin/system.py
@@ -46,8 +46,7 @@ class SystemPlugin(Plugin):
     def get_uptime(self, args):
         with open('/proc/uptime', 'r') as f:
             uptime_seconds = float(f.readline().split()[0])
-            uptime_string = str(timedelta(seconds = uptime_seconds))
-        return ["JamesII started " + self.utils.get_nice_age(self.core.startup_timestamp) + ", the system " + self.utils.get_nice_age(uptime_string)]
+        return ["JamesII started " + self.utils.get_nice_age(self.core.startup_timestamp) + ", the system " + self.utils.get_nice_age(uptime_seconds)]
 
     def start(self):
         try:

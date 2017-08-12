a445de990017bc33c1259070b0907bc0f339d13e
Marc Urben <aegnor@mittelerde.ch>
2017-08-12 22:45:23 +0200

    BT device detection is now optimized and more configurable

--- src/james/plugin/proximity.py
+++ src/james/plugin/proximity.py
@@ -261,6 +261,7 @@ class ProximityPlugin(Plugin):
             elif self.missingcount < int(self.config['miss_count']):
                 self.logger.info('Proximity missingcounter increased to %s of %s' %
                                  (self.missingcount, self.config['miss_count']))
+                self.oldstatus = True
             elif self.missingcount >= int(self.config['miss_count']):
                 message.append('Proximity is now watching!')
                 self.logger.info("Missingcounter reached its max (%s), sending proximity status: %s@%s" %

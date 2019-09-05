d5d51d6d0701a96dc2d09cc468c6e58d74f9d848
Marc Urben <aegnor@mittelerde.ch>
2019-09-06 00:07:03 +0200

    activating debug output due to systemd unit

--- src/james/jamesutils.py
+++ src/james/jamesutils.py
@@ -283,7 +283,7 @@ class JamesUtils(object):
 
             # screen handler
             streamhandler = logging.StreamHandler()
-            streamhandler.setLevel(logging.INFO)
+            streamhandler.setLevel(logging.DEBUG)
             streamhandler.setFormatter(screen_formatter)
             log.addHandler(streamhandler)
 

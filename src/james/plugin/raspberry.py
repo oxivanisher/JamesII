1f97619cc8d72c75500d97c95b85833a78cc198e
Marc Urben <aegnor@mittelerde.ch>
2020-04-19 19:06:40 +0200

    fixing pin pull up/down

--- src/james/plugin/raspberry.py
+++ src/james/plugin/raspberry.py
@@ -147,7 +147,9 @@ class RaspberryThread(PluginThread):
 
                 if button_pressed:
                     self.pin_state_cache['buttons'][pin] += 1
-                    self.logger.debug("Button press registered")
+                    if self.pin_state_cache['buttons'][pin] == 1:
+                        self.logger.debug("Button press registered")
+
                     if (self.pin_state_cache['buttons'][pin] % 100) == 0 or self.pin_state_cache['buttons'][pin] == 2:
                         if len(self.led_pins) > 1:
                             self.led_blink(1, 1)

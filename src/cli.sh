675f07e6749c2acaee5075834adaa2303fe6eb82
Marc Urben <aegnor@mittelerde.ch>
2021-10-19 01:59:56 +0200

    adding venv support

--- src/cli.sh
+++ src/cli.sh
@@ -3,6 +3,12 @@ INPWD=$(pwd)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-./cli.py $@
+if [ -f "../venv/bin/python" ];
+then
+	pwd
+	../venv/bin/python ./cli.py $@
+else
+	./cli.py $@
+fi	
 
 cd $INPWD >/dev/null 2>&1  

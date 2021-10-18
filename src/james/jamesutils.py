a10e224034076ba0f4c711e7c54fe3dc5cff1829
Marc Urben <aegnor@mittelerde.ch>
2021-10-18 18:06:38 +0200

    debugging proximity

--- src/james/jamesutils.py
+++ src/james/jamesutils.py
@@ -270,8 +270,7 @@ class JamesUtils(object):
         logger = self.getLogger('jamesutils', self.core.logger)
         logger.debug('popenAndWait: %s' % command)
         ret = subprocess.Popen(command, stderr=subprocess.PIPE, stdout=subprocess.PIPE).communicate()[0]
-        logger.debug('popenAndWait return: %s' % ret)
-        return ret.split("\n")
+        return ret.decode('UTF-8').split("\n")
 
     def getLogger(self, name, parent=None):
 

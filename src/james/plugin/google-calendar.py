1d7ed9a3aa9fdbeb62e2dfb1141badabf7e589e7
Marc Urben <aegnor@mittelerde.ch>
2023-04-19 02:15:28 +0200

    * implementing no_alarm_clock functionality with its own channel * implementing caching mechanism in google-calendar.py

--- src/james/plugin/google-calendar.py
+++ src/james/plugin/google-calendar.py
@@ -169,13 +169,13 @@ class GoogleCalendarPlugin(Plugin):
                     return_string = "Tomorrow "
             self.logger.debug("a")
             # check there is a "don't wake up" event present in google calendar
-            if event['summary'].lower() in self.config['no_alarm_clock'].lower() and happening_today:
+            if event['summary'].lower() in [x.lower() for x in self.config['no_alarm_clock']] and happening_today:
                 self.logger.info("Found a event which activates no_alarm_clock: %s" % event['summary'])
                 no_alarm_clock_active = True
 
             self.logger.debug("b")
             # ignore ignored_events from config
-            if event['summary'].lower() in self.config['ignored_events'].lower():
+            if event['summary'].lower() in [x.lower() for x in self.config['ignored_events']]:
                 self.logger.debug("ba")
                 self.logger.debug("Ignoring event because of ignored_events: %s" % event)
                 continue

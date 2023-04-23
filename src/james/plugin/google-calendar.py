b9319a40dc1ee26db544aa619317f22ea669a624
Marc Urben <aegnor@mittelerde.ch>
2023-04-23 02:58:03 +0200

    improving and fixing gcal

--- src/james/plugin/google-calendar.py
+++ src/james/plugin/google-calendar.py
@@ -75,15 +75,15 @@ class GoogleCalendarPlugin(Plugin):
     def fetch_events(self, calendar_id, page_token=None):
         events = {}
 
-        rfc3339_format = '%Y-%m-%dT%H:%M:%S.%f%z'
+        rfc3339_format = '%Y-%m-%dT%H:%M:%S.%f'
         here_now = datetime.now(self.timezone)
 
         midnight_today = datetime(here_now.year, here_now.month, here_now.day, tzinfo=self.timezone)
-        last_minute_today = datetime(here_now.year, here_now.month, here_now.day, 23, 59, 0, tzinfo=self.timezone)
-        last_minute_tomorrow = last_minute_today + timedelta(days=1)
+        last_second_today = datetime(here_now.year, here_now.month, here_now.day, 23, 59, 59, tzinfo=self.timezone)
+        last_second_tomorrow = last_second_today + timedelta(days=1)
 
         midnight_today_str = midnight_today.strftime(rfc3339_format)
-        last_second_tomorrow_str = last_minute_tomorrow.strftime(rfc3339_format)
+        last_second_tomorrow_str = last_second_tomorrow.strftime(rfc3339_format)
 
         self.logger.debug("fetching events from <%s> to <%s>" % (midnight_today_str, last_second_tomorrow_str))
 

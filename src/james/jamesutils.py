251ab3dc51a0c944e1a7baabd7ab9aa5dea18e87
Marc Urben <aegnor@mittelerde.ch>
2023-04-29 03:34:26 +0200

    debugging btproximity

--- src/james/jamesutils.py
+++ src/james/jamesutils.py
@@ -212,6 +212,21 @@ class JamesUtils(object):
         # http://code.activestate.com/recipes/358449-wake-on-lan/
         """ Switches on remote computers using WOL. """
 
+        def send_magic_packet(mac_address):
+            # Create a UDP socket
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+            # Construct the magic packet
+            mac_bytes = bytes.fromhex(mac_address)
+            magic_packet = b'\xff' * 6 + mac_bytes * 16
+
+            # Send the magic packet
+            sock.sendto(magic_packet, ('<broadcast>', 9))
+
+            # Close the socket
+            sock.close()
+
         # Check macaddress format and try to compensate.
         if len(macaddress) == 12:
             pass
@@ -222,21 +237,24 @@ class JamesUtils(object):
             raise ValueError('Incorrect MAC address format')
 
         # Pad the synchronization stream.
-        data = ''.join([str('FFFFFFFFFFFF'), str(macaddress) * 20])
-        send_data = str('')
+        # data = ''.join(['FFFFFFFFFFFF', macaddress * 20])
+        # send_data = ''
 
         logger.warning("c")
 
         # Split up the hex values and pack.
-        for i in range(0, len(data), 2):
-            send_data = ''.join([send_data, struct.pack('B', int(data[i: i + 2], 16))])
+        # for i in range(0, len(data), 2):
+        #     send_data = ''.join([send_data, struct.pack('B', int(data[i: i + 2], 16))])
 
         logger.warning("d")
 
         # Broadcast it to the LAN.
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        sock.sendto(send_data, ('<broadcast>', 7))
+        # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        # sock.sendto(send_data, ('<broadcast>', 7))
+
+        send_magic_packet(macaddress)
+        logger.warning("e")
 
     def convert_from_unicode(self, data):
         if isinstance(data, str):

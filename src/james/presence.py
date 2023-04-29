1680257004e1fe997140a987c591ee2e11190011
Marc Urben <aegnor@mittelerde.ch>
2023-04-29 02:36:56 +0200

    debugging btproximity

--- src/james/presence.py
+++ src/james/presence.py
@@ -42,7 +42,7 @@ class Presences(object):
     def check_timeouts(self):
         for presence in self.presences:
             if not presence.is_timeout_ok():
-                self.core.logger("Presence removed due to node not sending updates (presence_timeout):" % presence)
+                self.core.logger("Presence removed due to node not sending updates (presence_timeout):" % presence.dump())
                 self.presences.remove(presence)
 
     def get_present_users_here(self):
